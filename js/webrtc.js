fab62ecb64c0e06e1922e1dc4fa797263ac2d467
vaseninm <vasenin@pixelio.ru>
2014-05-13 16:08:13 +0400

    some configs ADD

--- js/webrtc.js
+++ js/webrtc.js
@@ -4,6 +4,46 @@
         host: 'localhost',
         port: '1488',
         secure: false,
+        iceServers: [
+            {url:'stun:stun.l.google.com:19302'},
+            {url:'stun:stun1.l.google.com:19302'},
+            {url:'stun:stun2.l.google.com:19302'},
+            {url:'stun:stun3.l.google.com:19302'},
+            {url:'stun:stun4.l.google.com:19302'},
+            {url:'stun:stun01.sipphone.com'},
+            {url:'stun:stun.ekiga.net'},
+            {url:'stun:stun.fwdnet.net'},
+            {url:'stun:stun.ideasip.com'},
+            {url:'stun:stun.iptel.org'},
+            {url:'stun:stun.rixtelecom.se'},
+            {url:'stun:stun.schlund.de'},
+            {url:'stun:stunserver.org'},
+            {url:'stun:stun.softjoys.com'},
+            {url:'stun:stun.voiparound.com'},
+            {url:'stun:stun.voipbuster.com'},
+            {url:'stun:stun.voipstunt.com'},
+            {url:'stun:stun.voxgratia.org'},
+            {url:'stun:stun.xten.com'},
+            {
+                url: 'turn:numb.viagenie.ca',
+                credential: 'muazkh',
+                username: 'webrtc@live.com'
+            },
+            {
+                url: 'turn:192.158.29.39:3478?transport=udp',
+                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                username: '28224511:1379330808'
+            },
+            {
+                url: 'turn:192.158.29.39:3478?transport=tcp',
+                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                username: '28224511:1379330808'
+            }
+        ],
+        maxVideoHeight: 640,
+        maxVideoWidth: 480,
+        minVideoFrameRate: 20,
+
         onGetLocalVideo: function (url) {},
         onCall: function (clientId) {},
         onServerConnect: function (connection, id, clients) {},
@@ -28,7 +68,16 @@
         var clientList = {};
 
         /* Инициализация */
-        navigator.getUserMedia({ audio: true, video: true }, function(stream) {
+        navigator.getUserMedia({
+            audio: true,
+            video: {
+                mandatory: {
+                    maxWidth: options.maxVideoWidth,
+                    maxHeight: options.maxVideoHeight,
+                    minFrameRate: options.minFrameRate
+                }
+            }
+        }, function(stream) {
             console.log('Получено лоакальное видео');
 
             localStream = stream;
@@ -130,7 +179,7 @@
         var getPeerConnection = function (clientId) {
             if (! clientList[clientId]) {
 
-                var pc = new PeerConnection(null);
+                var pc = new PeerConnection({iceServers: options.iceServers});
 
                 pc.addStream(localStream);
                 pc.onaddstream = function (event) {
@@ -142,7 +191,7 @@
                     options.onGetRemoteVideo(clientId, url);
                 };
                 pc.onicecandidate = function (iceCandidate) {
-                    console.log('Получен айс кандидат', iceCandidate);
+                    console.log('Получен айс кандидат от сервера');
 
                     if (! iceCandidate.candidate) return false;
 
